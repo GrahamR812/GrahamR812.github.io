bd6ac32f611f4d46b0014b9d56c8e94380370f66
Graham Richards <thegrahamrichards@gmail.com>
2026-09-24 10:10:47 -0700

    Added reappearing navbar when you scroll up.
    Added a can picked up image to engr7b
    
    Added citations to internet images in engr7b

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,33 @@
 fetch('/components/navbar.html')
     .then(response => response.text())
     .then(data => {
-    document.getElementById('navbar-container').innerHTML = data;
+        const navbarContainer = document.getElementById('navbar-container');
+        navbarContainer.innerHTML = data;
+
+        const navbar = document.querySelector('.navbar');
+        navbarContainer.style.height = `${navbar.offsetHeight}px`;
+        let previousScrollY = window.scrollY;
+
+        window.addEventListener('resize', () => {
+            navbarContainer.style.height = `${navbar.offsetHeight}px`;
+        });
+
+        window.addEventListener('scroll', () => {
+            const currentScrollY = window.scrollY;
+
+            if (currentScrollY <= 0) {
+                navbar.classList.remove('navbar-floating');
+                navbar.classList.remove('navbar-hidden');
+            } else {
+                navbar.classList.add('navbar-floating');
+            }
+
+            if (currentScrollY > previousScrollY + 8) {
+                navbar.classList.add('navbar-hidden');
+            } else if (currentScrollY < previousScrollY - 8) {
+                navbar.classList.remove('navbar-hidden');
+            }
+
+            previousScrollY = currentScrollY;
+        }, { passive: true });
 });
